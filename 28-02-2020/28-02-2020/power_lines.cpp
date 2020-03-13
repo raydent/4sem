9f9a0b06b48232ed676ac6f8e6d698d50e45e0e3
Raydent <benisgachi@gmail.com>
2020-03-13 19:15:29 +0300

    some progress

--- 28-02-2020/28-02-2020/power_lines.cpp
+++ 28-02-2020/28-02-2020/power_lines.cpp
@@ -3,8 +3,11 @@
 #include <map>
 #include <vector>
 #include <algorithm>
+#include <unordered_map>
+#include <iostream>
+#include <chrono>
 const float __CHARGE__ = 1;
-const int __LINE_NUMBER__ = 8;
+const int __LINE_NUMBER__ = 4;
 
 struct Charge {
 	Charge(sf::Vector2i v, bool b) : pos(v), sign(b) {};
@@ -13,12 +16,17 @@ struct Charge {
 };
 
 
+bool operator<(const sf::Vector2i& lhs, const sf::Vector2i& rhs) {
+	//return (lhs.x * lhs.x + lhs.y * lhs.y) < (rhs.x * rhs.x + rhs.y * rhs.y);
+	return (lhs.x < rhs.x) && (lhs.y < rhs.y);
+}
+
 class ChargeSystem {
 public:
 	void insert_pos(sf::Vector2i point) {
 		points.push_back(Charge{point, true});
 		sf::CircleShape s{ 25 };
-		s.setFillColor(sf::Color::Blue);
+		s.setFillColor(sf::Color::Red);
 		s.setPosition(point.x, point.y);
 		circles.push_back(s);
 		recalc();
@@ -32,35 +40,56 @@ public:
 		recalc();
 	}
 	void recalc() {
+		auto _zero_it_ = begin(points);
 		for (auto it = begin(points); it != end(points); it++) {
-			std::vector<std::vector<sf::Vertex>> v(__LINE_NUMBER__);
-			sf::Vector2i curpos = it->pos;
-			float one_angle = 360 / __LINE_NUMBER__;
-			int move = 5;
-			for (int i = 0; i < __LINE_NUMBER__; i++) {
-				v[i].push_back(sf::Vertex(sf::Vector2f(curpos.x + move * cos(i * one_angle), curpos.y + move * sin(i * one_angle)), sf::Color::Black));
-				sf::Vector2f cur_line_pos = v[i].back().position;
-				while (cur_line_pos.x >= 0 && cur_line_pos.x <= 1024 
-					&& cur_line_pos.y >= 0 && cur_line_pos.y <= 768) {
-					float Ex = 0, Ey = 0;
-					for (const auto& i : points) {
-						int sign = i.sign ? 1 : -1;
-						Ex += sign / ((i.pos.x - cur_line_pos.x) * (i.pos.x - cur_line_pos.x));
-						Ey += sign / ((i.pos.y - cur_line_pos.y) * (i.pos.y - cur_line_pos.y));
+			if (it->sign) {
+				std::vector<sf::VertexArray> v(__LINE_NUMBER__);
+				sf::Vector2i curpos = it->pos;
+				float one_angle = 360 / __LINE_NUMBER__;
+				int move = 5;
+				for (int i = 0; i < __LINE_NUMBER__; i++) {
+					v[i].append(sf::Vertex(sf::Vector2f(curpos.x + move * cos(i * one_angle), curpos.y + move * sin(i * one_angle)), sf::Color::Black));
+					sf::Vector2f cur_line_pos = v[i].operator[](v[i].getVertexCount()).position;
+					std::cout << cur_line_pos.x << " " << cur_line_pos.y << std::endl;
+					while (cur_line_pos.x >= 0 && cur_line_pos.x <= 1024
+						&& cur_line_pos.y >= 0 && cur_line_pos.y <= 768) {
+						//std::cout << "turn" << std::endl;
+						float Ex = 0, Ey = 0;
+						for (const auto& i : points) {
+							int sign = i.sign ? 1 : -1;
+							/*Ex += sign / ((i.pos.x - cur_line_pos.x) * (i.pos.x - cur_line_pos.x));
+							Ey += sign / ((i.pos.y - cur_line_pos.y) * (i.pos.y - cur_line_pos.y));*/
+							auto vec = cur_line_pos - static_cast<sf::Vector2f>(i.pos);
+							auto mod_ = sqrt(vec.x * vec.x + vec.y * vec.y);
+							auto E = sign / mod_;
+							Ex += E * vec.x / mod_;
+							Ey += E * vec.y / mod_;
+						}
+						float mod = sqrt(Ex * Ex + Ey * Ey);
+						v[i].append(sf::Vertex{ sf::Vector2f{cur_line_pos.x + move * Ex / mod, cur_line_pos.y + move * Ey / mod}, sf::Color::Black });
+						sf::Vector2f cur_line_pos = v[i].operator[](v[i].getVertexCount()).position;
 					}
-					float mod = sqrt(Ex * Ex + Ey * Ey);
-					v[i].push_back(sf::Vertex{ sf::Vector2f{cur_line_pos.x + Ex / mod, cur_line_pos.y + Ey / mod}, sf::Color::Black});
-					cur_line_pos = v[i].back().position;
+				}
+				size_t dif = it - _zero_it_;
+				std::cout << dif << std::endl;
+				if (charges.size() <= dif) {
+					charges.push_back(v);
+				}
+				else {
+					charges[it - _zero_it_] = v;
 				}
 			}
-			charges[it->pos] = v;
 		}
 	}
 	auto& get_map() {
 		return charges;
 	}
+	auto& get_circles() {
+		return circles;
+	}
 private:
-	std::map<sf::Vector2i, std::vector<std::vector<sf::Vertex>>> charges;
+	//std::map<sf::Vector2i, std::vector<std::vector<sf::Vertex>>> charges;
+	std::vector<std::vector<sf::VertexArray>> charges;
 	std::vector<Charge> points;
 	std::vector<sf::CircleShape> circles;
 };
@@ -69,6 +98,8 @@ int main() {
 	//std::list<sf::Vector2i> points;
 	window.clear(sf::Color::White);
 	ChargeSystem cs;
+	auto c = std::chrono::steady_clock::now();
+	float time_dif = 100;
 	while (window.isOpen()) {
 		sf::Event event;
 		window.pollEvent(event);
@@ -80,15 +111,21 @@ int main() {
 		default:
 			break;
 		}
-		window.display();
-	}
-	if (sf::Mouse::isButtonPressed(sf::Mouse::Left)) {
-		auto pos = sf::Mouse::getPosition(window);
-		cs.insert_pos(pos);
-	}
-	for (auto it1 = begin(cs.get_map()); it1 != end(cs.get_map()); it1++) {
-		for (auto it2 = begin(it1->second); it2 != end(it1->second); it2++) {
-			window.draw(it2, it2->size(), sf::Lines);
+		if (sf::Mouse::isButtonPressed(sf::Mouse::Left)) {
+			if (std::chrono::duration_cast<std::chrono::milliseconds>(std::chrono::steady_clock::now() - c).count() > time_dif) {
+				auto pos = sf::Mouse::getPosition(window);
+				cs.insert_pos(pos);
+				c = std::chrono::steady_clock::now();
+			}
 		}
+		for (auto it = begin(cs.get_circles()); it != end(cs.get_circles()); it++) {
+			window.draw(*it);
+		}
+		for (auto it1 = begin(cs.get_map()); it1 != end(cs.get_map()); it1++) {
+			for (auto it2 = begin(*it1); it2 != end(*it1); it2++) {
+				window.draw(*it2);
+			}
+		}
+		window.display();
 	}
 }
